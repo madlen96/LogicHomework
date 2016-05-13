164b614d574c072ca124a572f96d2864c007a00a
madlen96 <magdalenka.k96@gmail.com>
2016-05-13 19:26:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ for i in sys.argv[1:]:
         print("Example: \n python program.py arg1 arg2 arg3 arg4 arg5 arg6")
         sys.exit()   
     counter += 1
-        
 if counter < 6:
     print(" Too few arguments. \n' --info 'to get more information")
     sys.exit()
 elif counter > 6:
     print(" Too much arguments. \n' --info ' to get more information")
     sys.exit()
-
 arg = [ int(x) for x in sys.argv[1:] ]
-
-if (arg[0] >= 20 or arg[1] <= 10 or
-    arg[2] % 2 != 0 or arg[3] % 5 != 0 or
-    arg[4] % arg[5] != 0 or arg[5] <= 0):
+if (arg[0] < 20 and
+    arg[1] > 10 and
+    arg[2] % 2 == 0 and
+    arg[3] % 5 == 0 and
+    arg[4] % arg[5] == 0 and
+    arg[5] > 0):
     print("1")    
 else:
     print("0")
